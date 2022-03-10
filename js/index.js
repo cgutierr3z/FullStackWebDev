c636ca2f818b37a37a806e717fde944ab71243b7
Carlos Gutierrez <cgutierrez@utp.edu.co>
2022-03-10 14:19:53 -0500

    remove contacto buttons, fix js events in index.js, ready to deploy

--- js/index.js
+++ js/index.js
@@ -5,4 +5,21 @@ $(function(){
   $(".carousel").carousel({
     interval: 3333
   });
+
+  $('#exampleModal').on('show.bs.modal', function(event){
+    console.log('modal is show, button is disabled');
+
+    $('#contactoBtn').removeClass('btn-outline-success');
+    $('#contactoBtn').addClass('btn-primary');
+    $('#contactoBtn').prop('disabled', true)
+  })
+
+  $('#exampleModal').on('hidden.bs.modal', function(event){
+    console.log('modal is hidden, button is enabled');
+
+    $('#contactoBtn').removeClass('btn-primary');
+    $('#contactoBtn').addClass('btn-outline-success');
+    $('#contactoBtn').prop('disabled', false)
+  })
+
 });
